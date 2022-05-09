a66cad6d981c69130fcb10c342f2d069d34b00f4
Slava Timbaliuc <slava.timbaliuc@gmail.com>
2022-05-09 11:47:08 -0400

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x43280f209F7ca46cfA80a660190B0E0dBd02d920";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
